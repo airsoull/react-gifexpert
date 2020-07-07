07942a17e8d98b6261c4df12797182a794723a1a
Cristian <cristian@magnet.cl>
2020-07-07 11:01:57 -0400

    chore: rename build folder to docs

---
+++ docs/precache-manifest.86f009e6b5c590ad5af38aba396f6bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6006d364cb71dc0e498c4630c00416c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d14b8987b98aea00dd1b",
+    "url": "/static/css/main.565d8a8e.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14b8987b98aea00dd1b",
+    "url": "/static/js/main.59f05aeb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
