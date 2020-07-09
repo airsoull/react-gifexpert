3255aefb6edd374e10174f97d92580595ee3d9bd
Cristian <cristian@magnet.cl>
2020-07-09 10:48:11 -0400

    test: initial config for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Enzyme from 'enzyme';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Adapter from 'enzyme-adapter-react-16';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createSerializer } from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
